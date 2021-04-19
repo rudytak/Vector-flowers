a51f91910172560a2b2223b418725da5af89f04d
rudytak <75335232+rudytak@users.noreply.github.com>
2021-04-19 18:27:29 +0200

    Got rid of drop due to incompability

--- sketch.js
+++ sketch.js
@@ -1,29 +1,33 @@
 var pdf;
+var c;
 
 var img;
 var _json;
 
+function preload() {
+    img = loadImage("img.png");
+    _json = loadJSON("branches.json")
+}
+
 function setup() {
     // If we use SVG Renderer, then the PDF generated will be vector
     // Note that to use SVG Renderer, you must include p5.svg library
-    var c = createCanvas(600, 600, SVG);
+    c = createCanvas(600, 600, SVG);
     pdf = createPDF();
-    pdf.beginRecord();
-
-    c.drop(fileDrop);
-}
-
-function fileDrop(file) {
-    if (!img) {
-        img = createImg(file.data, "").hide();
-    }
 }
 
 function draw() {
+    pdf.beginRecord();
+
     background(255);
 
 
 
     noLoop();
     pdf.save();
+
+}
+
+function drawBranch(bJSON) {
+
 }
