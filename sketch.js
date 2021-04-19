afd9ee3377c07fa28d3efabaed0f115fd5aeafd8
rudytak <75335232+rudytak@users.noreply.github.com>
2021-04-19 16:50:12 +0200

    File drop prep

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
 var pdf;
 
+var img;
+var _json;
+
 function setup() {
     // If we use SVG Renderer, then the PDF generated will be vector
     // Note that to use SVG Renderer, you must include p5.svg library
-    createCanvas(600, 200, SVG);
+    var c = createCanvas(600, 600, SVG);
     pdf = createPDF();
     pdf.beginRecord();
+
+    c.drop(fileDrop);
+}
+
+function fileDrop(file) {
+    if (!img) {
+        img = createImg(file.data, "").hide();
+    }
 }
 
 function draw() {
     background(255);
+
 
 
     noLoop();
